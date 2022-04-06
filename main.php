6b1f38ef3a3ce1391d3cb0ece9983b690b93fc61
diamondmuffin01 <80565512+diamondmuffin01@users.noreply.github.com>
2022-04-06 13:00:32 -0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,29 @@
+<?
+
+$mType = stripslashees($_GET['mType']);
+$mFile = stripslashees($_GET['mFile']);
+$title = stripslashees($_GET['title']);
+$bgColor = stripslashees($_GET['bgColor']);
+
+?>
+
+<title><> echo $title; ?></title>
+<?
+	if ($bgColor=null) {
+		print '<body style="background-color:#7487a7;">'; }
+	else {
+		print '<body style="background-color:#' .bgColor . ';">';
+	}
+?>
+
+<link href='https://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
+<div style="margin:120px auto 0; width: 70%; padding:54px; background-color:#ffffff'; text-align:center;
+border:#999999 solid 2px; border-radius:33px;">
+<?
+	print '<h2 style="font-family:Courgette; color:#888888;">' .$title . "</h2>\n";
+	print '<p><' .$mType . ' style="width:80%;" controls>< source src="' . $mType . '/' . $mFile . '"></'
+	. $mType . '></p>';
+?>
+</div>
+</body>
+		
