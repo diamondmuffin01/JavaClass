d8bcff9c02eac583525d2de5430cade626a3bb4d
diamondmuffin01 <80565512+diamondmuffin01@users.noreply.github.com>
2022-04-07 21:00:12 -0700

    Update main.php

--- main.php
+++ main.php
@@ -1,19 +1,16 @@
-<?
-$mType = stripslashees($_GET['mType']);
-$mFile = stripslashees($_GET['mFile']);
-$title = stripslashees($_GET['title']);
-$bgColor = stripslashees($_GET['bgColor']);
+lol
+<?php
+$mType = stripslashes($_GET['mType']);
+$mFile = stripslashes($_GET['mFile']);
+$title = stripslashes($_GET['title']);
 ?>
-
-<title><?  echo $title; ?></title>
-
+<title> <?php  echo $title; ?></title>
 <body style="background-color:#7487a7;">
-
 <link href='https://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
-<div style="margin:120px auto 0; width: 70%; padding:54px; background-color:#ffffff; text-align:center;
-border:#999999 solid 2px; border-radius:33px;">
-<?
-	print '<h2 style="font-family:Courgette; color:#888888;">' . $title. "</h2>\n";
-	print '<p> <' . $mType . ' style="width:80%;" controls><source src="' . $mType . '/' . $mFile . '"></'
-	. $mType . '></p>';
-?> </div></body>
+<div style="margin:120px auto 0; width: 70%; padding:54px; background-color:#ffffff; text-align:center; border:#999999 solid:2px; border-radius:33px;">
+<?php
+print '<h2 style="font-family:Courgette; color:#888888;"> ' . $title . "</h2>\n";
+print '<p><' . $mType . ' style="width:80%;" controls><source src="' . $mFile . '"></' . $mType . '></p>';
+?>
+</div>
+</body>
